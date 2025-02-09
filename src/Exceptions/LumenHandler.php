d441547807a205e12328cc7bd82b78258f03ec7f
Ahmed Elrayes <30704271+Ahmed-Elrayes@users.noreply.github.com>
2025-02-09 23:53:15 +0200

    Refactor exception handling for JSON responses
    Unified and streamlined the exception handling logic for JSON requests in `Handler.php` and `LumenHandler.php`. Consolidated checks for request type and exception types to improve readability and maintainability.

--- src/Exceptions/LumenHandler.php
+++ src/Exceptions/LumenHandler.php
@@ -7,12 +7,11 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\ValidationException;
-use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Collection;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use MA\LaravelApiResponse\Traits\APIResponseTrait;
 use ParseError;
 use Psr\Log\LogLevel;
@@ -81,44 +80,42 @@ class LumenHandler extends ExceptionHandler
      */
     public function render($request, Throwable $e): \Symfony\Component\HttpFoundation\Response
     {
-        // Not found http exception
-        if ($e instanceof NotFoundHttpException && $request->wantsJson()) {
-            return $this->apiResponse(['type' => 'not found']);
-        }
-
-        // Method not allowed http exception
-        if ($e instanceof MethodNotAllowedHttpException && $request->wantsJson()) {
-            return $this->apiResponse(['type' => 'not found']);
-        }
-
-        // Status code 0
-        if ($e instanceof HttpException && $request->wantsJson()) {
-            return $this->apiBadRequest();
-        }
-
-        // Server error
-        if (($e instanceof Error || $e instanceof ParseError) && $request->wantsJson()) {
-            // Check if app debug is enabled to return traces
-            if (config('app.debug')) {
-                // Set data
-                $data = new Collection([
-                    'exception' => get_class($e),
-                    'file' => $e->getFile(),
-                    'line' => $e->getLine(),
-                    'trace' => $e->getTrace(),
-                ]);
-
-                return $this->apiResponse([
-                    'type' => 'server error',
-                    'message' => $e->getMessage(),
-                    'data' => $data,
-                ]);
+        // Check if request expects json
+        if ($request->expectsJson()) {
+            // Not found http exception OR Method not allowed http exception
+            if ($e instanceof NotFoundHttpException || $e instanceof MethodNotAllowedHttpException) {
+                return $this->apiNotFound();
             }
 
-            // Return server error response
-            return $this->apiResponse([
-                'type' => 'server error'
-            ]);
+            // Status code 0
+            if ($e instanceof HttpException) {
+                return $this->apiBadRequest();
+            }
+
+            // Server error
+            if (($e instanceof Error || $e instanceof ParseError)) {
+                // Check if app debug is enabled to return traces
+                if (config('app.debug')) {
+                    // Set data
+                    $data = new Collection([
+                        'exception' => get_class($e),
+                        'file' => $e->getFile(),
+                        'line' => $e->getLine(),
+                        'trace' => $e->getTrace(),
+                    ]);
+
+                    return $this->apiResponse([
+                        'type' => 'server error',
+                        'message' => $e->getMessage(),
+                        'data' => $data,
+                    ]);
+                }
+
+                // Return server error response
+                return $this->apiResponse([
+                    'type' => 'server error'
+                ]);
+            }
         }
 
         return parent::render($request, $e);
